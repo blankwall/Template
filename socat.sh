b6979e4efbaaf07b145a8f6015a6a5cfe4558b1e
blankwall <1blankwall1@gmail.com>
2014-06-03 15:46:04 -0400

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 EXPECTED_ARGS=1
+EXPECTED_ARG=2
 E_BADARGS=65
 
-if [ $# -ne $EXPECTED_ARGS ]
+if [ $# -lt $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` {arg}"
-  exit $E_BADARGS
+  	echo "Usage: `basename $0` binary [optional port #]"
+  	exit $E_BADARGS
 fi
 
-echo "PORT: 2323 Binary: $1"
-socat TCP-LISTEN:2323,reuseaddr,fork EXEC:./$1
+if [ $# -ne $EXPECTED_ARG ]
+then
+	echo "PORT: 2323  Binary: $1"
+	socat TCP-LISTEN:2323,reuseaddr,fork EXEC:./$1
+
+else   
+	echo "PORT: $2  Binary: $1"
+	socat TCP-LISTEN:$2,reuseaddr,fork EXEC:./$1
+fi
