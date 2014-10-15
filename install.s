4765dd6f710b96cf84381c766ac3da87a4e2794b
blankwall <1blankwall1@gmail.com>
2014-10-15 11:40:51 -0400

    Update

--- install.s
+++ install.s
@@ -7,3 +7,6 @@ fi
 
 chmod +x gdb
 sudo mv gdb /usr/bin/gdb
+echo "set auto-load safe-path /" >> ~/.gdbinit;
+git clone https://github.com/longld/peda.git ~/peda
+git clone https://github.com/blankwall/Template.git ~/Template
