1734d6ddb4b29496b7e7fcf9665f3b28d0d10f25
blankwall <1blankwall1@gmail.com>
2015-02-24 15:22:33 -0500

    Update install.s

--- install.s
+++ install.s
@@ -10,4 +10,5 @@ sudo mv gdb /usr/bin/gdb
 sudo apt-get install -y vim tmux git socat
 echo "set auto-load safe-path /" >> ~/.gdbinit
 git clone https://github.com/longld/peda.git ~/peda
+echo "source ~/peda/peda.py" >> ~/.gdbinit
 git clone https://github.com/blankwall/Template.git ~/Template
